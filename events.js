8f5e2ad07b86549355cbbc15d7c45e3d47f4049f
sebz <smellems@gmail.com>
2021-02-16 12:45:47 -0500

    Update with Prod code

--- events.js
+++ events.js
@@ -1,7 +1,9 @@
 import wixData from 'wix-data';
 import wixStores from 'wix-stores-backend';
-import wixBilling from 'wix-billing-backend';
-import { invoices } from 'wix-billing-backend';
+//import wixBilling from 'wix-billing-backend';
+//import { invoices } from 'wix-billing-backend';
+import { getSecret } from 'wix-secrets-backend';
+import Iconv from 'iconv-lite';
 
 // Test
 export function wixStores_onCartCreated(event) {
@@ -9,7 +11,116 @@ export function wixStores_onCartCreated(event) {
     console.log("onCartCreated " + total);
 }
 
+export async function wixStores_onOrderPaid(event) {
+    
+    let OrderNb = event.number;
+
+    // Logging
+    console.log(event);
+    
+    const DropboxAccessToken = await getSecret("DropboxAccessToken");
+
+    let options = {
+        "suppressAuth": true
+    };
+
+    return wixData.query("Stores/Orders")
+        .eq("number", OrderNb)
+        .find(options)
+        .then((results) => {
+            let order = results.items[0];
+            let orderDate = new Date();
+            let items = "";
+            for (let i = 0; i < order.lineItems.length; i++) {
+                items += "<item>" +
+                    "<Itemid>" + order.lineItems[i].sku + "</Itemid>" +
+                    "<Description>" + order.lineItems[i].name + "</Description>" +
+                    "<Quantity>" + order.lineItems[i].quantity + "</Quantity>" +
+                    "<Price>" + order.lineItems[i].priceData.totalPrice + "</Price>" +
+                    "</item>";
+            }
+
+            let xmlstr = "<?xml version=\"1.0\" encoding=\"ISO-8859-1\"?>" +
+                "<commande>" +
+                "<TimeOrdered>" + orderDate.getHours() + ":" + orderDate.getMinutes() + ":" + orderDate.getSeconds() + "</TimeOrdered>" +
+                "<DateOrdered>" + orderDate.getFullYear() + "-" + orderDate.getMonth() + "-" + orderDate.getDate() + "</DateOrdered>" +
+                "<TimeDelivery>" + orderDate.getHours() + ":" + orderDate.getMinutes() + ":" + orderDate.getSeconds() + "</TimeDelivery>" +
+                "<DateDelivery>" + orderDate.getFullYear() + "-" + orderDate.getMonth() + "-" + orderDate.getDate() + "</DateDelivery>" +
+                "<Function>0</Function>" +
+                "<SendToKitchen>0</SendToKitchen>" +
+                "<CloseInvoice>0</CloseInvoice>" +
+                "<NoTax>0</NoTax>" +
+                "<Customer>" +
+                "<Phone>" + order.buyerInfo.phone.replace(/ /gi, "").replace(/-/gi, "") + "</Phone>" +
+                "<Name>" + order.buyerInfo.firstName + " " + order.buyerInfo.lastName + "</Name>" +
+                "<Address>" +
+                "<Number></Number>" +
+                "<Street>" + order.billingInfo.address.addressLine + ((order.billingInfo.address.addressLine2 !== undefined) ? ", " + order.billingInfo.address.addressLine2 : "") + "</Street>" +
+                "<AptNum></AptNum>" +
+                "<Near></Near>" +
+                "<CityState>" + order.billingInfo.address.city + ", " + order.billingInfo.address.subdivision + "</CityState>" +
+                "<ZipCode>" + order.billingInfo.address.postalCode.replace(/ /gi, "") + "</ZipCode>" +
+                "<Email>" + order.buyerInfo.email + "</Email>" +
+                "</Address>" +
+                "<Remark>" + ((order.buyerNote !== undefined) ? order.buyerNote : "") + "</Remark>" +
+                "<Remark2/>" +
+                "<Remark3/>" +
+                "<Remark4/>" +
+                "<Tax1/>" +
+                "<Tax2/>" +
+                "<Tax3/>" +
+                "<Tax4/>" +
+                "</Customer>" +
+                "<Orders>" +
+                "<Order>" +
+                items +
+                "<Charges>" +
+                "<Service>" +
+                "<Amount>" + order.totals.subtotal + "</Amount>" +
+                "</Service>" +
+                "<Gratuity>" +
+                "<Amount>0</Amount>" +
+                "</Gratuity>" +
+                "<Delivery>" +
+                "<Amount>" + order.totals.shipping + "</Amount>" +
+                "</Delivery>" +
+                "</Charges>" +
+                "</Order>" +
+                "</Orders>" +
+                "</commande>";
+
+            // Logging
+            console.log(OrderNb + " - " + order.buyerInfo.firstName + " " + order.buyerInfo.lastName + " - " + order.totals.subtotal);
+
+            return fetch("https://content.dropboxapi.com/2/files/upload", {
+                    "method": "post",
+                    "headers": {
+                        "Authorization": "Bearer " + DropboxAccessToken,
+                        "Content-Type": "application/octet-stream",
+                        "Charset": "ISO-8859-1",
+                        "Accept": "application/json",
+                        "Dropbox-API-Arg": "{\"path\": \"/ORDER-" + OrderNb + ".xml\",\"mode\": \"add\",\"autorename\": false,\"mute\": true,\"strict_conflict\": false}"
+                    },
+                    "body": Iconv.encode(xmlstr, "ISO-8859-1")
+                })
+                .then((result) => {
+                    console.log(result.json());
+                    if (result.ok) {
+                        return true;
+                    } else {
+                        return false;
+                    }
+                });
+
+        })
+        .catch((error) => {
+            console.log(error);
+            return false;
+        });
+}
+
 /**** New Order ****/
+/*
 export function wixStores_onNewOrder(event) {
 
     const now = new Date()
@@ -26,14 +137,10 @@ export function wixStores_onNewOrder(event) {
     let lineItems = [];
     let bulk = false;
 
-    let options = {
-        "suppressAuth": true
-    };
-
     wixData.query("Stores/Orders")
         .include("lineItems")
         .eq("number", event.number)
-        .find(options)
+        .find()
         .then((results) => {
             if (results.items.length > 0) {
                 let orderData = results.items[0];
@@ -87,32 +194,33 @@ export function wixStores_onNewOrder(event) {
     /*if (!bulk && event.paymentStatus == "NOT_PAID") {
       invoices.sendInvoice(newInvoiceId, event.buyerInfo.email);
     }*/
+/*
 
-    // Save XML order file
+    // Save XML order file on BEST
 
     return true;
 }
 
 /**** Invoice Paid ****/
+/*
 export function wixBilling_onInvoicePaid(event) {
 
     // Mark Order as paid
-    let options = {
-        "suppressAuth": true
-    };
-
     wixData.query("Stores/Orders")
         .eq("number", event.title.substr(15))
-        .find(options)
+        .find()
         .then((results) => {
             if (results.items.length > 0) {
                 let item = results.items[0];
                 item.paymentStatus = "PAID";
-                wixData.update("Stores/Orders", item, options);
+                wixData.update("Stores/Orders", item);
             }
         })
         .catch((err) => {
             console.log("errorMsg " + err);
         });
 
+    // Save XML order file on BEST
+
 }
+*/
--- http-functions.js
+++ http-functions.js
@@ -1,39 +1,66 @@
 import { ok, serverError } from 'wix-http-functions';
 import { getSecret } from 'wix-secrets-backend';
+import { fetch } from 'wix-fetch';
 import wixStores from 'wix-stores-backend';
 import wixData from 'wix-data';
+import Iconv from 'iconv-lite';
 
-export function updateVariants(product, choiceNb, available, newPrice) {
+export function updateVariants(product, choiceNb, visible, newPrice, choicesType) {
 
     let variantInfo = {
         trackQuantity: false,
         variants: [{
             variantId: product.variants[choiceNb]._id,
-            inStock: available
+            inStock: visible
         }]
     };
 
     wixStores.updateInventoryVariantFieldsByProductId(product._id, variantInfo)
         .then(() => {
-
-            const choice = product.variants[choiceNb].choices.Grandeur;
-            const price = (newPrice * product.productOptions.Grandeur.choices[choiceNb].value / 1000).toFixed(2);
-            const visible = available;
-            let variantData = [{
-                visible,
-                price,
-                "choices": {
-                    "Grandeur": choice
-                }
-            }];
-
-            wixStores.updateVariantData(product._id, variantData);
+            if (choicesType !== "Unit") {
+                let choice = product.variants[choiceNb].choices[choicesType];
+                let price = (newPrice * product.productOptions[choicesType].choices[choiceNb].value / 1000).toFixed(2);
+                let variantData = [{
+                    visible,
+                    price,
+                    "choices": {
+                        [choicesType]: choice
+                    }
+                }];
+                wixStores.updateVariantData(product._id, variantData);
+            }
         });
+}
+
+export function updateCollectionsAndVariants(department, options, product, variants, choicesType, available, price) {
+    wixData.query("Stores/Collections")
+        .startsWith("name", department)
+        .find(options)
+        .then((results2) => {
+            if (results2.items.length > 0) {
+                let collection = results2.items[0];
+                wixStores.addProductsToCollection(collection._id, [product._id]);
+            }
+        });
+
+    if (variants !== "") {
+        let nbChoices = product.productOptions[choicesType].choices.length;
+        for (let i = 0; i < nbChoices; i++) {
+            updateVariants(product, i, available, price, choicesType);
+        }
+    } else if (!available) {
+        updateVariants(product, 0, false, price, "Unit");
+    }
 }
 
 export async function post_updateProduct(request) {
 
     const BestPosApiKey = await getSecret("BestPosApiKey");
+
+    const capitalize = (s) => {
+        if (typeof s !== 'string') return ''
+        return s.charAt(0).toUpperCase() + s.slice(1)
+    }
 
     let response = {
         "headers": {
@@ -45,148 +72,240 @@ export async function post_updateProduct(request) {
     return request.body.json()
         .then((body) => {
 
+            //console.log(body);
+            //response.body = { "message": body };
+            //return ok(response);
+
             if (body.key === BestPosApiKey) {
+
+                let sku = body.sku
+                let name = capitalize(body.name.toLowerCase().replace(/á/g, "à"));
+                let price = Number(body.price).toFixed(2);
+                let quantity = Number(body.quantity);
+                let department = capitalize(body.department.toLowerCase());
+                //let group = body.group;
+                //let supplier = body.supplier;
+                //let tax1 = body.tax1;
+                //let tax2 = body.tax2;
+                let sizeChart = body.SizeChart;
+                let brand = capitalize(body.brand.toLowerCase());
+                let variants = body.variants;
+
+                let available = (quantity > 1) ? true : false;
+                let choicesType = (sizeChart !== "UNIT") ? "Couleur" : "Grandeur";
+                let nameWeb = ((brand !== "") ? brand + " - " : "") + name;
+
+                let productOptions = {
+                    "250-500-1000": {
+                        "Poid": {
+                            "choices": [{
+                                    "description": "250 g",
+                                    "value": "250"
+                                },
+                                {
+                                    "description": "500 g",
+                                    "value": "500"
+                                },
+                                {
+                                    "description": "1 Kg",
+                                    "value": "1000"
+                                }
+                            ]
+                        }
+                    },
+                    "50-100": {
+                        "Poid": {
+                            "choices": [{
+                                    "description": "50 g",
+                                    "value": "50"
+                                },
+                                {
+                                    "description": "100 g",
+                                    "value": "100"
+                                }
+                            ]
+                        }
+                    }
+                };
+
+                if (variants === "250-500-1000" || variants === "50-100") {
+                    productOptions = productOptions[variants];
+                    choicesType = "Poid";
+                } else if (variants !== "" && parseInt(variants, 10) > 0) {
+                    productOptions = {
+                        "Poid": {
+                            "choices": [{
+                                "description": variants + " g",
+                                "value": variants
+                            }]
+                        }
+                    };
+                    choicesType = "Poid";
+                } else if (variants !== "" && variants.split("--").length > 0) {
+                    let choices = [];
+                    for (let i = 0; i < variants.split("--").length; i++) {
+                        choices.push({
+                            "description": capitalize(variants.split("--")[i].toLowerCase()),
+                            "value": "1000"
+                        });
+                    }
+
+                    productOptions = {
+                        [choicesType]: {
+                            "choices": choices
+                        }
+                    };
+                } else {
+                    variants = "";
+                }
+
+                // Logging
+                console.log(sku + "-" + name + "-<" + variants + ">");
 
                 let options = {
                     "suppressAuth": true
                 };
 
-                let productOptions = {
-                    "Grandeur": {
-                        "choices": [{
-                                "description": "250 g",
-                                "value": "250",
-                                //"inStock": false, // readonly for create
-                                //"visible": false // readonly for create
-                            },
-                            {
-                                "description": "500 g",
-                                "value": "500",
-                                //"inStock": false, // readonly for create
-                                //"visible": false // readonly for create
-                            }
-                        ]
-                    }
-                };
-
-                //response.body = { "message": body };
-                //return ok(response);
-
-                let itemId = body.ItemId
-                let name = body.name.toLowerCase();
-                let price = body.price;
-                let quantity = body.quantity;
-                //let department = body.department;
-                //let group = body.group;
-                //let supplier = body.supplier;
-                //let tax1 = body.tax1;
-                //let tax2 = body.tax2;
-
-                let available = false;
-                if (Number(quantity) > 1) {
-                    available = true;
-                }
-
-                // Find Product based on SKU (ItemId)
+                // Find Product based on SKU
                 return wixData.query("Stores/Products")
-                    .eq("sku", itemId)
+                    .eq("sku", sku)
                     .find(options)
                     .then((results) => {
-
-                        //response.body = { "message": results };
-                        //return ok(response);
 
                         if (results.items.length > 0) {
                             let product = results.items[0];
 
-                            //response.body = { "message": product };
-                            //return ok(response);
-
                             // Update Product
 
-                            return wixStores.updateProductFields(product._id, {
-                                    "name": name,
-                                    //"description": body.description,
-                                    "price": price
-                                })
-                                .then((updatedProduct) => {
+                            return wixStores.resetVariantData(product._id)
+                                .then(() => {
+                                    return wixStores.deleteProductOptions(product._id)
+                                        .then(() => {
+                                            let productFields = {
+                                                "name": nameWeb,
+                                                "manageVariants": false,
+                                                "price": price
+                                            };
 
-                                    // Update Collections
+                                            if (variants !== "") {
+                                                productFields = {
+                                                    "name": nameWeb,
+                                                    "manageVariants": true,
+                                                    "price": price,
+                                                    "productOptions": productOptions
+                                                }
+                                            }
+                                            return wixStores.updateProductFields(product._id, productFields)
+                                                .then((updatedProduct) => {
 
-                                    //let collectionId = query
-                                    //wixStores.addProductsToCollection(collectionId, [updatedProduct._id]);
+                                                    updateCollectionsAndVariants(department, options, updatedProduct, variants, choicesType, available, price);
 
-                                    // Update Variants
-
-                                    updateVariants(updatedProduct, 0, available, price);
-                                    updateVariants(updatedProduct, 1, available, price);
-
-                                    response.body = {
-                                        "message": "Product has been updated"
-                                    };
-                                    return ok(response);
-                                })
-                                .catch((error) => {
-                                    response.body = {
-                                        "errorUpdate": error
-                                    };
+                                                })
+                                                .then(() => {
+                                                    response.body = { "message": "Product " + sku + " has been updated" };
+                                                    return ok(response);
+                                                }).catch((error) => {
+                                                    response.body = { "errorUpdate": error };
+                                                    return serverError(response);
+                                                });
+                                        }).catch((error) => {
+                                            response.body = { "errorDeleteOptions": error };
+                                            return serverError(response);
+                                        });
+                                }).catch((error) => {
+                                    response.body = { "errorResetVariants": error };
                                     return serverError(response);
                                 });
 
                         } else {
 
                             // Create Product
+                            let productFields = {
+                                "sku": sku,
+                                "name": nameWeb,
+                                "price": price,
+                                "manageVariants": false,
+                                "productType": "physical",
+                                "visible": true
+                            };
 
-                            return wixStores.createProduct({
-                                    "sku": itemId,
-                                    "name": name,
-                                    //"description": body.description,
+                            if (variants !== "") {
+                                productFields = {
+                                    "sku": sku,
+                                    "name": nameWeb,
                                     "price": price,
                                     "productOptions": productOptions,
                                     "manageVariants": true,
                                     "productType": "physical",
                                     "visible": true
-                                })
+                                }
+                            }
+                            return wixStores.createProduct(productFields)
                                 .then((newProduct) => {
 
-                                    // Update Collections
+                                    updateCollectionsAndVariants(department, options, newProduct, variants, choicesType, available, price);
 
-                                    // Update Variants
-
-                                    updateVariants(newProduct, 0, available, body.price);
-                                    updateVariants(newProduct, 1, available, body.price);
-
-                                    response.body = {
-                                        "message": "Product has been created"
-                                    };
+                                })
+                                .then(() => {
+                                    response.body = { "message": "Product " + sku + " has been created" };
                                     return ok(response);
                                 })
                                 .catch((error) => {
-                                    response.body = {
-                                        "errorCreate": error
-                                    };
+                                    response.body = { "errorCreate": error };
                                     return serverError(response);
                                 });
                         }
                     })
                     .catch((error) => {
-                        response.body = {
-                            "errorQuery": error
-                        };
+                        response.body = { "errorQuery": error };
                         return serverError(response);
                     });
             }
 
-            response.body = {
-                "error": "Not Permitted"
-            };
+            response.body = { "error": "Not Permitted" };
             return serverError(response);
 
         })
         .catch((error) => {
-            response.body = {
-                "errorJSON": error
-            };
+            response.body = { "errorJSON": error };
             return serverError(response);
         });
 }
+
+export async function get_testOnOrderPaid(request) {
+
+    let OrderNb = 123456;
+
+    // Logging
+    console.log(request);
+
+    // Copy events function below to test Dropbox XML file //
+
+}
+
+/* test function
+export function get_testQueryOrder(request) {
+
+    let response = {
+        "headers": {
+            "Content-Type": "application/json"
+        }
+    };
+
+    let options = {
+        "suppressAuth": true
+    };
+
+    return wixData.query("Stores/Orders")
+        .eq("number", 12345)
+        .find(options)
+        .then((results) => {
+            let order = results.items[0];
+            response.body = { "message": order };
+            return ok(response);
+        })
+        .catch((error) => {
+            response.body = { "errorQuery": error };
+            return serverError(response);
+        });
+}*/
